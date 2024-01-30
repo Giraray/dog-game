f6216dba594f186436df21c3827521e444b39da6
Giraray <119466153+Giraray@users.noreply.github.com>
2024-01-30 16:55:18 +0100

    new film grain shader!
    added film grain
    fixed window resize function

--- src/main.js
+++ src/main.js
@@ -1,283 +1,320 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import * as CANNON from 'cannon-es'
-import CannonDebugger from 'cannon-es-debugger'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
+  import * as THREE from 'three'
+  import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+  import * as CANNON from 'cannon-es'
+  import CannonDebugger from 'cannon-es-debugger'
+  import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+  import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
 
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
-import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader.js'
+  import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+  import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+  import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
+  import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader.js'
+  import {FilmPass} from 'three/examples/jsm/postprocessing/FilmPass.js'
 
-import * as Shaders from './assets/shaders.js'
+  import * as Shaders from './assets/shaders.js'
 
-const scene = new THREE.Scene()
+  function onWindowResize(){
 
-const world = new CANNON.World({
-  gravity: new CANNON.Vec3(0, -9.82, 0)
-})
-const slipperyMaterial = new CANNON.Material('slippery')
-const groundMaterial = new CANNON.Material('ground')
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
 
-const ground_slippery = new CANNON.ContactMaterial(groundMaterial, slipperyMaterial, {
-  friction: 0,
-  restitution: 0,
-  contactEquationStiffness: 1e8,
-  contactEquationRelaxation: 3
-})
-world.addContactMaterial(ground_slippery)
-
-const ground = new CANNON.Body({
-  type: CANNON.Body.STATIC,
-  shape: new CANNON.Plane(),
-  material: groundMaterial
-})
-ground.quaternion.setFromEuler(-Math.PI / 2, 0, 0)
-world.addBody(ground)
-
-const cannonDebugger = new CannonDebugger(scene, world)
-
-// camera
-const testCamera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000)
-testCamera.position.set(100, 300, -100)
-
-const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(100, 300, -100)
-const helper = new THREE.CameraHelper(camera);
-//scene.add(helper)
-
-// renderer
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.getElementById('bg')
-})
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(window.innerWidth, window.innerHeight)
-
-// composer
-const composer = new EffectComposer(renderer)
-composer.addPass(new RenderPass(scene, camera))
-composer.addPass(new ShaderPass(FXAAShader))
-composer.addPass(new ShaderPass({
-  vertexShader: Shaders.chromaVS,
-  fragmentShader: Shaders.chromaFS,
-  uniforms: {
-    map: composer.readBuffer.texture,
-    resolution: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) }
+      renderer.setSize( window.innerWidth, window.innerHeight );
 
   }
-  //uniforms: { resolution: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) } }
-}))
-console.log(composer)
+  window.addEventListener( 'resize', onWindowResize, false );
 
-// gridHelper
-const gridHelper = new THREE.GridHelper(2000, 50)
-scene.add(gridHelper)
+  const scene = new THREE.Scene()
 
-//lights
-const ambientLight = new THREE.AmbientLight(0x99ff99, 1)
-scene.add(ambientLight)
-
-const topLight = new THREE.PointLight(0xffffff, 10000)
-topLight.position.y = 300
-topLight.position.x = 50
-scene.add(topLight)
-
-const objLoader = new OBJLoader()
-
-// player
-const player = new CANNON.Body({
-  mass: 10,
-  shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1)),
-  material: slipperyMaterial
-})
-player.position.set(0, 10, 0)
-world.addBody(player)
-
-//model
-let playerModel
-
-const dogMaterial = new THREE.ShaderMaterial({
-  uniforms: {},
-  vertexShader: Shaders.dogVS,
-  fragmentShader: Shaders.dogFS
-})
-
-const placeHolderMaterial = new THREE.MeshToonMaterial({ color: 0xffffff })
-
-objLoader.load('assets/doggo_merged.obj',
-  function (obj) {
-    obj.traverse(function (child) {
-      if (child instanceof THREE.Mesh) {
-        child.material = placeHolderMaterial;
-      }
-    });
-    scene.add(obj);
-    playerModel = obj;
-    console.log(obj)
-  },
-  function (xhr) {
-    console.log((xhr.loaded / xhr.total * 100) + "% loaded")
-  },
-  function (err) {
-    console.error("Error loading 'doggo_merged.obj': " + err)
-  }
-);
-
-const tGeometry = new THREE.TorusGeometry(10, 2, 16, 20);
-const tMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-const torus = new THREE.Mesh(tGeometry, tMaterial);
-torus.quaternion.set(0.6, 0, 0, 1)
-scene.add(torus);
-
-const cubeGeometry = new THREE.BoxGeometry(100, 100, 100);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-scene.add(cube);
-cube.position.set(200, 50, 100)
-
-//controls
-
-const controls = new OrbitControls(camera, renderer.domElement);
-
-// player controls
-let upPressed = false
-let leftPressed = false
-let downPressed = false
-let rightPressed = false
-let movementSpeed = 300
-
-let cameraLock = true
-window.addEventListener('keydown', (e) => {
-  switch (e.key.toLowerCase()) {
-    case 'arrowup':
-    case 'w':
-      upPressed = true
-      break;
-    case 'arrowleft':
-    case 'a':
-      leftPressed = true
-      break;
-    case 'arrowdown':
-    case 's':
-      downPressed = true
-      break;
-    case 'arrowright':
-    case 'd':
-      rightPressed = true
-      break;
-  }
-})
-
-window.addEventListener('keyup', (e) => {
-  switch (e.key.toLowerCase()) {
-    case 'arrowup':
-    case 'w':
-      upPressed = false
-      break;
-    case 'arrowleft':
-    case 'a':
-      leftPressed = false
-      break;
-    case 'arrowdown':
-    case 's':
-      downPressed = false
-      break;
-    case 'arrowright':
-    case 'd':
-      rightPressed = false
-      break;
-  }
-})
-
-function playerMovement() {
-  let pVelocity = player.velocity
-
-  if (upPressed == true && downPressed != true) {
-
-    pVelocity.x = movementSpeed * Math.sin(rotationRadians)
-    pVelocity.z = movementSpeed * Math.cos(rotationRadians)
-  }
-  if (leftPressed == true && rightPressed != true) {
-    pVelocity.x = movementSpeed * Math.cos(rotationRadians)
-    pVelocity.z = movementSpeed * Math.sin(-rotationRadians)
-  }
-  if (downPressed == true && upPressed != true) {
-    pVelocity.x = -movementSpeed * Math.sin(rotationRadians)
-    pVelocity.z = -movementSpeed * Math.cos(rotationRadians)
-  }
-  if (rightPressed == true && leftPressed != true) {
-    pVelocity.x = -movementSpeed * Math.cos(rotationRadians)
-    pVelocity.z = -movementSpeed * Math.sin(-rotationRadians)
-  }
-}
-
-function cameraRotateWithPlayer() {
-  const idealOffset = new THREE.Vector3(
-    playerModel.position.x - 300 * Math.sin(rotationRadians),
-    playerModel.position.y + 250,
-    playerModel.position.z - 40 * Math.cos(rotationRadians)
-  )
-
-  const idealLookat = new THREE.Vector3(
-    playerModel.position.x + 250 * Math.sin(rotationRadians),
-    playerModel.position.y,
-    playerModel.position.z + 250 * Math.cos(rotationRadians)
-  )
-
-  camera.position.lerp(idealOffset, 1)
-  camera.lookAt(idealLookat)
-  testCamera.lookAt(idealLookat.x, idealLookat.y, idealLookat.z)
-}
-
-let rotationRadians = 0
-let upAngleRadians = 0
-const pointerControls = new PointerLockControls(camera, document.body)
-document.getElementById('lock').addEventListener('click', function () {
-  pointerControls.lock()
-  let spins = 0
-  const pi = Math.PI
-  addEventListener('mousemove', (e) => {
-    let mouseX = e.movementX * 0.001
-    let mouseY = e.movementY * 0.001
-
-    upAngleRadians = camera.rotation.x
-    camera.rotation.x -= mouseY
-
-    playerModel.rotation.y -= mouseX
-    rotationRadians = playerModel.rotation.y - spins * pi * 2
-
-    if (Math.abs(rotationRadians) > pi * 2) {
-      rotationRadians += Math.sign(rotationRadians) * pi * 2
-      spins += Math.sign(rotationRadians)
-    }
-    cameraRotateWithPlayer(rotationRadians)
+  const world = new CANNON.World({
+    gravity: new CANNON.Vec3(0, -9.82, 0)
   })
-})
+  const slipperyMaterial = new CANNON.Material('slippery')
+  const groundMaterial = new CANNON.Material('ground')
 
-//gameloop
-function animate() {
-  requestAnimationFrame(animate, 33)
+  const ground_slippery = new CANNON.ContactMaterial(groundMaterial, slipperyMaterial, {
+    friction: 0,
+    restitution: 0,
+    contactEquationStiffness: 1e8,
+    contactEquationRelaxation: 3
+  })
+  world.addContactMaterial(ground_slippery)
 
-  world.fixedStep()
-  cannonDebugger.update()
+  const ground = new CANNON.Body({
+    type: CANNON.Body.STATIC,
+    shape: new CANNON.Plane(),
+    material: groundMaterial
+  })
+  ground.quaternion.setFromEuler(-Math.PI / 2, 0, 0)
+  world.addBody(ground)
 
-  playerModel.position.copy(player.position)
-  playerModel.position.y -= 2
+  const cannonDebugger = new CannonDebugger(scene, world)
 
-  controls.update();
+  // camera
+  const testCamera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000)
+  testCamera.position.set(100, 300, -100)
 
-  playerMovement()
+  const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000)
+  camera.position.set(100, 300, -100)
+  const helper = new THREE.CameraHelper(camera);
+  //scene.add(helper)
 
-  //if (cameraLock == true) { cameraRotateWithPlayer() }
+  // renderer
+  const bg = document.getElementById('bg')
+  bg.width = window.innerWidth
+  bg.height = window.innerHeight
 
-  composer.render(scene, camera)
-}
+  const renderer = new THREE.WebGLRenderer({
+    canvas: bg
+  })
 
-animate()
+  // composer + shader materials
+  const composer = new EffectComposer(renderer)
 
-window.addEventListener("resize", () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-});
+  const uniforms = {
+    tDiffuse: composer.readBuffer.texture,
+    resolution: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
+    frame: { value: 0 }
+  }
+
+  const chromaShader = new THREE.ShaderMaterial({
+    vertexShader: Shaders.chromaVS,
+    fragmentShader: Shaders.chromaFS,
+    uniforms: {
+      tDiffuse: uniforms.tDiffuse,
+      resolution: uniforms.resolution
+    }
+  })
+
+  const grainShader = new THREE.ShaderMaterial({
+    vertexShader: Shaders.chromaVS,
+    fragmentShader: Shaders.grainFS,
+    uniforms: {
+      tDiffuse: uniforms.tDiffuse,
+      resolution: uniforms.resolution,
+      frame: uniforms.frame
+    }
+  })
+
+  composer.addPass(new RenderPass(scene, camera))
+  composer.addPass(new ShaderPass(FXAAShader))
+  composer.addPass(new ShaderPass(chromaShader))
+  composer.addPass(new ShaderPass(grainShader))
+
+  // gridHelper
+  const gridHelper = new THREE.GridHelper(2000, 50)
+  scene.add(gridHelper)
+
+  //lights
+  const ambientLight = new THREE.AmbientLight(0x99ff99, 1)
+  scene.add(ambientLight)
+
+  const topLight = new THREE.PointLight(0xffffff, 10000)
+  topLight.position.y = 300
+  topLight.position.x = 50
+  scene.add(topLight)
+
+  const objLoader = new OBJLoader()
+
+  // player
+  const player = new CANNON.Body({
+    mass: 10,
+    shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1)),
+    material: slipperyMaterial
+  })
+  player.position.set(0, 10, 0)
+  world.addBody(player)
+
+  //model
+  let playerModel
+
+  const dogMaterial = new THREE.ShaderMaterial({
+    uniforms: {},
+    vertexShader: Shaders.dogVS,
+    fragmentShader: Shaders.dogFS
+  })
+
+  const placeHolderMaterial = new THREE.MeshToonMaterial({ color: 0xffffff })
+
+  objLoader.load('assets/doggo_merged.obj',
+    function (obj) {
+      obj.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+          child.material = placeHolderMaterial;
+        }
+      });
+      scene.add(obj);
+      playerModel = obj;
+      console.log(obj)
+    },
+    function (xhr) {
+      console.log((xhr.loaded / xhr.total * 100) + "% loaded")
+    },
+    function (err) {
+      console.error("Error loading 'doggo_merged.obj': " + err)
+    }
+  );
+
+  const tGeometry = new THREE.TorusGeometry(10, 2, 16, 20);
+  const tMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  const torus = new THREE.Mesh(tGeometry, tMaterial);
+  torus.quaternion.set(0.6, 0, 0, 1)
+  scene.add(torus);
+
+  const cubeGeometry = new THREE.BoxGeometry(100, 100, 100);
+  const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  scene.add(cube);
+  cube.position.set(200, 50, 100)
+
+  //controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  // player controls
+  const playerControls = {
+    up: false,
+    left: false,
+    down: false,
+    right: false
+  }
+  let movementSpeed = 300
+
+  let cameraLock = true
+  window.addEventListener('keydown', (e) => {
+    switch (e.key.toLowerCase()) {
+      case 'arrowup':
+      case 'w':
+        playerControls.up = true
+        break;
+      case 'arrowleft':
+      case 'a':
+        playerControls.left = true
+        break;
+      case 'arrowdown':
+      case 's':
+        playerControls.down = true
+        break;
+      case 'arrowright':
+      case 'd':
+        playerControls.right = true
+        break;
+    }
+  })
+
+  window.addEventListener('keyup', (e) => {
+    switch (e.key.toLowerCase()) {
+      case 'arrowup':
+      case 'w':
+        playerControls.up = false
+        break;
+      case 'arrowleft':
+      case 'a':
+        playerControls.left = false
+        break;
+      case 'arrowdown':
+      case 's':
+        playerControls.down = false
+        break;
+      case 'arrowright':
+      case 'd':
+        playerControls.right = false
+        break;
+    }
+  })
+
+  function playerMovement() {
+    let pVelocity = player.velocity
+    let movementMult
+    const pc = playerControls
+    pVelocity.x = 0
+    pVelocity.y = 0
+    pVelocity.z = 0
+
+    if([pc.up, pc.left, pc.down, pc.right].filter(Boolean).length > 1) {
+      movementMult = 0.5
+    }
+    else {
+      movementMult = 1
+    }
+
+    if (pc.up == true && pc.down != true) {
+      pVelocity.x += movementSpeed * Math.sin(rotationRadians) * movementMult
+      pVelocity.z += movementSpeed * Math.cos(rotationRadians)* movementMult
+    }
+    if (pc.left == true && pc.right != true) {
+      pVelocity.x += movementSpeed * Math.cos(rotationRadians)* movementMult
+      pVelocity.z += movementSpeed * Math.sin(-rotationRadians)* movementMult
+    }
+    if (pc.down == true && pc.up != true) {
+      pVelocity.x += -movementSpeed * Math.sin(rotationRadians)* movementMult
+      pVelocity.z += -movementSpeed * Math.cos(rotationRadians)* movementMult
+    }
+    if (pc.right == true && pc.left != true) {
+      pVelocity.x += -movementSpeed * Math.cos(rotationRadians)* movementMult
+      pVelocity.z += -movementSpeed * Math.sin(-rotationRadians)* movementMult
+    }
+  }
+
+  function cameraRotateWithPlayer() {
+    const idealOffset = new THREE.Vector3(
+      playerModel.position.x - 250 * Math.sin(rotationRadians),
+      playerModel.position.y + 250,
+      playerModel.position.z - 250 * Math.cos(rotationRadians)
+    )
+
+    const idealLookat = new THREE.Vector3(
+      playerModel.position.x + 250 * Math.sin(rotationRadians),
+      playerModel.position.y,
+      playerModel.position.z + 250 * Math.cos(rotationRadians)
+    )
+
+    camera.position.lerp(idealOffset, 1)
+    camera.lookAt(idealLookat)
+    testCamera.lookAt(idealLookat.x, idealLookat.y, idealLookat.z)
+  }
+
+  let rotationRadians = 0
+  let upAngleRadians = 0
+  const pointerControls = new PointerLockControls(camera, document.body)
+  document.getElementById('lock').addEventListener('click', function () {
+    pointerControls.lock()
+    let spins = 0
+    const pi = Math.PI
+    addEventListener('mousemove', (e) => {
+      let mouseX = e.movementX * 0.001
+      let mouseY = e.movementY * 0.001
+
+      upAngleRadians = camera.rotation.x
+      camera.rotation.x -= mouseY
+
+      playerModel.rotation.y -= mouseX
+      rotationRadians = playerModel.rotation.y - spins * pi * 2
+
+      if (Math.abs(rotationRadians) > pi * 2) {
+        rotationRadians += Math.sign(rotationRadians) * pi * 2
+        spins += Math.sign(rotationRadians)
+      }
+      cameraRotateWithPlayer(rotationRadians)
+    })
+  })
+
+  //gameloop
+  function animate() {
+    requestAnimationFrame(animate, 33)
+    uniforms.frame.value = renderer.info.render.frame
+
+    world.fixedStep()
+    cannonDebugger.update()
+
+    playerModel.position.copy(player.position)
+    playerModel.position.y -= 2
+
+
+    playerMovement()
+
+    //controls.update();
+    if (cameraLock == true) { cameraRotateWithPlayer() }
+
+    composer.render(scene, camera)
+  }
+
+  animate()
